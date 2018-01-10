e92894729a91e4981398380adc59d58fe2a00b77
Arne Johannes Holmin <arnejh@imr.no>
2018-01-10 16:00:28 +0100

    After building Rstox 1.8

--- build.R
+++ build.R
@@ -37,7 +37,7 @@ dir <- "~/Documents/Produktivt/Prosjekt/R-packages/Rstox/Rstox"
 #buildRstox(dir, version="1.7.1", Rversion="3.4.2", pckversion=list(data.table="1.10.4-3"), official=FALSE, check=FALSE)
 
 # Build 1.7.2:
-buildRstox(dir, version="1.7.2", Rversion="3.4.2", pckversion=list(data.table="1.10.4-3"), official=FALSE, check=FALSE)
+#buildRstox(dir, version="1.7.2", Rversion="3.4.2", pckversion=list(data.table="1.10.4-3"), official=FALSE, check=FALSE)
 
 # Build 1.8:
-#buildRstox(dir, version="1.8", Rversion="3.4.3", pckversion=list(data.table="1.10.4-3"), official=TRUE, check=FALSE)
+buildRstox(dir, version="1.8", Rversion="3.4.3", pckversion=list(data.table="1.10.4-3"), official=TRUE, check=TRUE)
