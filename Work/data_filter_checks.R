1581cd6bebc11afc43a9d380cfd371cdf6dafa97
edvinf <edvin.fuglebakk@gmail.com>
2018-06-26 11:11:06 +0200

    Polished some plots. Removed catchsamples with no ind. from second plot.

--- Work/data_filter_checks.R
+++ Work/data_filter_checks.R
@@ -28,7 +28,12 @@ plot_length_measurements <- function(catchsample, title="lengdemål", xlab="# fa
   names(tt)[names(tt)==""] <- blankcode
   labels[labels$name=="","name"] <- blankcode
   
-  barplot(tt, xlab=xlab, names=paste(labels$description, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)
+  if (length(tt)>1){
+    barplot(tt, xlab=xlab, names=paste(labels$description, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)  
+  }
+  else{
+    pie(tt, labels=paste(labels$description, " (", names(tt), ")", sep=""))  
+  }
 }
 
 #' Composition of product types samples
@@ -45,17 +50,57 @@ plot_product_types <- function(catchsample, title="produkttyper", xlab="# fangst
   names(tt)[names(tt)==""] <- blankcode
   labels[labels$name=="","name"] <- blankcode
   
-  barplot(tt, xlab=xlab, names=paste(labels$description, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)
+  if (length(tt)>1){
+    barplot(tt, xlab=xlab, names=paste(labels$description, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)  
+  }
+  else{
+    pie(tt, labels=paste(labels$description, " (", names(tt), ")", sep=""))    
+  }
+}
+
+#' Plots partition of catchsamples which have individals, ages or only total catch sampled
+#' @param catchsample
+#' @param individual
+#' @param has_a_name label for bar of catchsamples with age read individuals
+#' @param has_i_name label for bar of catchsamples with individuals sampled, but no age read
+#' @param notname label for bar of catchsamples with no individuals sampled
+#' @param ylab label for y axis
+#' @param title title for plot
+plot_individuals <- function(catchsample, individual, has_a_name="alder", has_i_name="ind. u alder", notname="kun fangst", ylab="# fangstprøver", title="Individprøvetaking"){
+  ages <- individual[!is.na(individual$age),]
+  has_i <- merge(catchsample, unique(individual[,c("species", "serialno", "samplenumber")]), by=c("species", "serialno", "samplenumber"))
+  has_a <- merge(catchsample, unique(ages[,c("species", "serialno", "samplenumber")]), by=c("species", "serialno", "samplenumber"))
+  n_not_i <- nrow(catchsample) - nrow(has_i)
+  n_i_not_a <- nrow(has_i) - nrow(has_a)
+  n_a <- nrow(has_a)
+  barplot(c(n_a, n_i_not_a, n_not_i), names=c(has_a_name, has_i_name, notname), ylab=ylab, main=title)
 }
 
 #' Weight length relations in the data set
 plot_age_length <- function(individual, xlab="alder", ylab="lengde", title="Alle individer"){
-  plot(individual$age, individual$length, xlab=xlab, ylab=ylab, main=title)
+  
+  plot(individual$age, individual$length, xlab=xlab, ylab=ylab, main=title, pch=".")
 }
 
 #' Age length relations in the data set
-plot_weight_length <- function(individual, xlab="vekt", ylab="lengde", title="Alle individer"){
-  plot(individual$weight, individual$length, xlab=xlab, ylab=ylab, main=title)
+#' @param individual
+#' @param xlab label for x-axis (weight)
+#' @param ylab label for y-axis (length)
+#' @param title title for plot
+#' @param density logical: if T, data points will be overlayed with density plot for densest 1-alpha fraction
+#' @param alpha see density
+plot_weight_length <- function(individual, xlab="vekt", ylab="lengde", title="Alle individer", density=T, alpha=0.01){
+  plot(individual$weight, individual$length, xlab=xlab, ylab=ylab, main=title, pch=".")
+  
+  if (density){
+    m<-kde2d(individual$weight, individual$length, n=100)
+    
+    ss <- sort(m$z)
+    cs <- cumsum(ss)
+    limz <-ss[cs<=alpha]
+    pp<-RColorBrewer::brewer.pal(name="Greys",n=9)[2:9]
+    image(m, col=pp, zlim=c(limz[length(limz)],max(m$z)+min(m$z)), add=T)
+  }
 }
 
 
@@ -83,7 +128,12 @@ plot_sample_types <- function(catchsample, title="prøvetyper", xlab="# fangstpr
   names(tt)[names(tt)==""] <- blankcode
   labels[labels$name=="","name"] <- blankcode
   
-  barplot(tt, xlab=xlab, names=paste(labels$shortname, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)
+  if (length(tt)>1){
+    barplot(tt, xlab=xlab, names=paste(labels$shortname, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)    
+  }
+  else{
+    pie(tt, labels=paste(labels$shortname, " (", names(tt), ")", sep=""))  
+  }
 }
 
 #' Composition of station types (Stasjons: stasjonstype) in data
@@ -109,7 +159,13 @@ plot_station_types <- function(station, title="statsjonstyper", xlab="# stasjone
   names(tt)[names(tt)==""] <- blankcode
   labels[labels$name=="","name"] <- blankcode
   
-  barplot(tt, xlab=xlab, names=paste(labels$shortname, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)
+  if(length(tt)>1){
+    barplot(tt, xlab=xlab, names=paste(labels$shortname, " (", names(tt), ")", sep=""), horiz = T, las=1, main=title, cex.names = cex.names)  
+  }
+  else{
+    pie(tt, labels=paste(labels$shortname, " (", names(tt), ")", sep=""))
+  }
+  
   
 }
 
@@ -123,7 +179,12 @@ plot_taxa_comp <- function(catchsample, title="taxa", xlab="# fangstprøver", bl
 
   tt <- table(as.character(catchsample$noname))
 
-  barplot(tt, xlab=xlab, names=names(tt), horiz = T, las=1, main=title, cex.names = cex.names)
+  if (length(tt)>1){
+    barplot(tt, xlab=xlab, names=names(tt), horiz = T, las=1, main=title, cex.names = cex.names)  
+  }
+  else{
+    pie(tt)
+  }
     
 }
 
@@ -142,7 +203,7 @@ plot_taxa_comp <- function(catchsample, title="taxa", xlab="# fangstprøver", bl
 #' @param disccol
 #' @param retaincol
 #' @param unkwoncol color to use when sampled fraction is not coded in data.
-plot_catch_fractions <- function(station, catchsample, title="Fangstfraksjoner samplet\n(platform/kvalitet/gruppe)", xlab="# fangstprøver", allname="All fangst", landname="Landet", retainname="Beholdt", discname="Ikke beholdt", allcol="red3", disccol="red", landcol="blue4", retaincol="blue", unkowncol="white", blankcode=default_blankcode){
+plot_catch_fractions <- function(station, catchsample, title="Fangstfraksjoner\n(platform/kvalitet/gruppe)", xlab="# fangstprøver", allname="All fangst", landname="Landet", retainname="S. landet", discname="S. ikke landet", allcol="red3", disccol="red", landcol="blue4", retaincol="blue", unkowncol="white", blankcode=default_blankcode){
   station <- merge(station, catchsample, by=c("cruise", "serialno"), suffixes = c(".station", ".catchsample"))
   station[is.na(station$trawlquality), "trawlquality"] <- rep(blankcode, sum(is.na(station$trawlquality)))
   station[is.na(station$group), "group"] <- rep(blankcode, sum(is.na(station$group)))
@@ -174,35 +235,36 @@ plot_catch_fractions <- function(station, catchsample, title="Fangstfraksjoner s
   barplot(counts$count, names=counts$label, col=counts$color, horiz=T, las=1, xlab=xlab, main=title)
   
   leg <- unique(counts[,c("color", "catchrep")])
-  legend("topright", fill=leg$color, legend=leg$catchrep)
+  legend("topright", fill=c(unkowncol, landcol, retaincol, disccol, allcol), legend=c(blankcode, landname, retainname, discname, allname))
 }
 
-#' Plots overview of potenital filtering parameters in samples
-plot_sample_comp <- function(station, catchsample){
+#' Plots overview of potential filtering parameters in samples
+plot_sample_comp <- function(catchsample, individual){
+  catchsample <- merge(catchsample, unique(individual[,c("serialno","species","samplenumber")]), by=c("serialno","species","samplenumber"))
   old.par <- par(no.readonly = T)
   par(mfrow=c(2,2))
-  par(mar=c(5.1,7,4.1,2.1))
-  plot_station_types(station)
   par(mar=c(5.1,12,4.1,2.1))
-  plot_sample_types(catchsample)
+  plot_sample_types(catchsample, xlab="# fangstprøver m/ind")
+  par(mar=c(5.1,7,4.1,2.1))
+  plot_product_types(catchsample, xlab="# fangstprøver m/ind")
+  par(mar=c(5.1,14,4.1,2.1))
+  plot_length_measurements(catchsample, xlab="# fangstprøver m/ind")
+  par(mar=c(5.1, 4.1,4.1,2.1))
+  plot_weight_length(individual, title="Alle ind. 1% ekstr, obs", alpha=0.01, density=T)
+  par(old.par)
+}
+
+plot_measurement_comp <- function(station, catchsample, individual){
+  old.par <- par(no.readonly = T)
+  par(mfrow=c(2,2))
+  par(mar=c(5.1,4.1,4.1,2.1))
+  plot_individuals(catchsample, individual)
   par(mar=c(5.1,7,4.1,2.1))
   plot_taxa_comp(catchsample)
   par(mar=c(5.1,12,4.1,2.1))
   plot_catch_fractions(station, catchsample)
-  par(old.par)
-}
-
-plot_measurement_comp <- function(catchsample, individual){
-  old.par <- par(no.readonly = T)
-  par(mfrow=c(2,2))
   par(mar=c(5.1,7,4.1,2.1))
-  plot_product_types(catchsample)
-  par(mar=c(5.1,14,4.1,2.1))
-  plot_length_measurements(catchsample)
-  par(mar=c(5.1, 4.1,4.1,2.1))
-  plot_weight_length(individual)
-  par(mar=c(5.1,4.1,4.1,2.1))
-  plot_age_length(individual)
+  plot_station_types(station)
   par(old.par)
 }
 
@@ -210,7 +272,7 @@ plot_measurement_comp <- function(catchsample, individual){
 # Funksjoner for Landings
 #
 
-# artssammensetning, produkttyper for manglende rundvekter, kvotetyper, nasjonskoder, manglende regmerker
+# artssammensetning, produkttyper for manglende rundvekter, kvotetyper, nasjonskoder fartøy, nasjonskoder landet
 
 #
 # Test-eksempler
@@ -233,11 +295,11 @@ runbl <- function(){
 }
 
 ex <- function(baselineOutput){
-  stations <- baselineOutput$outputData$FilterBiotic$FilterBiotic_BioticData_FishStation.txt
+  station <- baselineOutput$outputData$FilterBiotic$FilterBiotic_BioticData_FishStation.txt
   catchsample <- baselineOutput$outputData$FilterBiotic$FilterBiotic_BioticData_CatchSample.txt
   individual <- baselineOutput$outputData$FilterBiotic$FilterBiotic_BioticData_Individual.txt
-  plot_sample_comp(stations, catchsample) 
-  plot_measurement_comp(catchsample, individual)
+  plot_measurement_comp(station, catchsample, individual)
+  plot_sample_comp(catchsample, individual) 
 }
-baselineOutput <- runbl()
+#baselineOutput <- runbl()
 ex(baselineOutput)
