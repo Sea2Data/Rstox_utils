bfd8e1002b1d7ae2c1b5896f98b6e42343d62862
edvinf <edvin.fuglebakk@gmail.com>
2018-09-12 15:24:13 +0200

    Moved stuff to Rstox

--- Work/reportRECA.R
+++ Work/reportRECA.R
@@ -1,35 +1,4 @@
 library(Rstox)
-srcdir <- "/Users/a5362/code/github/Rstox_utils/Work"
-source(file.path(srcdir, "plot_results_ECA.R"))
-source(file.path(srcdir, "plotWrapper.R"))
 
-#' Generates plots and reports from RECA prediction
-#' @param projectname name of stox project
-#' @param verbose logical, if TRUE info is written to stderr()
-#' @param format function defining filtetype for plots, supports grDevices::pdf, grDevices::png, grDevices::jpeg, grDevices::tiff, grDevices::bmp
-#' @param ... parameters passed on plot function and format
-plotRECAresults <- function(projectName, verbose=F, format="png", ...){
-  rundata <- loadProjectData(projectName, var="runRECA")
-  prep <- loadProjectData(projectName, var="prepareRECA")
-  
-  if (format=="png"){
-    #dimension in pixels
-    width=5000
-    height=5000
-    res=500
-  }
-  if (format=="pdf"){
-    #dimension in inches
-    width=10
-    height=10
-    res=NULL
-  }
-  
-  formatPlot(projectname, "RECA_results", function(){plot_RECA_results_panel(rundata$runRECA$pred, prep$prepareRECA$StoxExport$biotic, ...)}, verbose=verbose, format=format, height=height, width=width, res=res, ...)
-  
-  warning("Implement save catch matrix")
-  warning("Fix defaults for units kt and mt. Consider adding lengthunits")
-  #save catch matrix
-}
-
-plotRECAresults(projectName)
+warning("Figure out proper name in Rstox")
+plotRECAresults(projectname)
--- Work/runECA.R
+++ Work/runECA.R
@@ -1,94 +1,8 @@
 library(eca)
 library(Rstox)
 
-#
-# workarounds
-#
-
-fix_in_prep_agelength<-function(AgeLength){
-  ## Select covariates - not use haulweight and boat now
-  newAgeLength <- AgeLength
-  #newAgeLength$info["spatial", "CAR"] <- 1
-
-  return(newAgeLength)  
-}
-fix_in_prep_weightlength <- function(WeightLength){
-  newWeightLength <- WeightLength
-  #newWeightLength$info["spatial", "CAR"] <- 1
-  return(newWeightLength)
-}
-fix_in_prep_landings <- function(Landings){
-  newLandings <- Landings
-
-  return(newLandings)
-}
-
-#
-# /workarounds
-#
-
-get_default_data_dir <- function(projectname, recadir=getProjectPaths(projectname)$RDataDir){
-  return(file.path(recadir, "reca", "datafiles"))
-}
-
 burnindefault=100
 samplesdefault=400
-thindefault=1
-defaultfitfile="fit"
-defaultpredfile="pred"
-defaultlgamodel="log-linear"
-defaultCC=TRUE
-defaultCCError=FALSE
-age.error.default=FALSE
-
-#' run fit and prediction. Save results to project data 'runRECA'
-#' parameters not described below are defined in eca.estimate and eca.predict
-#' @param export_only if not NULL this indicates that eca should not be run, but all parameters should be exported to the file export_only
-runRECA <- function(projectname, burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault, thin=thindefault, fitfile=defaultfitfile, predfile=defaultpredfile, lgamodel=defaultlgamodel, CC=defaultCC, CCError=defaultCCError, seed=NULL, age.error=age.error.default, export_only=NULL){
-  # Sett kjøreparametere her, sett dataparametere i prep_eca
-    prepdata <- loadProjectData(projectname, var="prepareRECA")   
-    prepareRECA <- prepdata$prepareRECA
-    if (is.null(prepdata)){
-      stop("Could not load project data")
-    }
-    GlobalParameters <- prepareRECA$GlobalParameters
-    AgeLength <- prepareRECA$AgeLength
-    WeightLength <- prepareRECA$WeightLength
-    Landings <- prepareRECA$Landings
-
-  GlobalParameters$caa.burnin <- burnin
-  GlobalParameters$burnin <- caa.burnin
-  GlobalParameters$nSamples <- nSamples
-  GlobalParameters$thin <- thin
-  GlobalParameters$fitfile <- fitfile
-  GlobalParameters$predictfile <- predfile
-  GlobalParameters$lgamodel <- lgamodel
-  GlobalParameters$CC <- CC
-  GlobalParameters$CCerror <- defaultCCError
-  GlobalParameters$age.error=age.error
-  
-  if (is.null(seed)){
-    seed=""
-  }
-  GlobalParameters$seed <- seed
-  
-  AgeLength <- fix_in_prep_agelength(AgeLength)
-  WeightLength <- fix_in_prep_weightlength(WeightLength)
-  Landings <- fix_in_prep_landings(Landings)
-
-  if (!is.null(export_only)){
-    save(GlobalParameters, AgeLength, WeightLength, Landings, file=export_only)
-  }
-  else{
-    ## Estimate model
-    fit <- eca.estimate(AgeLength,WeightLength,Landings,GlobalParameters)
-    
-    ## Predict
-    pred <- eca.predict(AgeLength,WeightLength,Landings,GlobalParameters)
-    
-    setProjectData(projectName=projectname, var=list(fit=fit, pred=pred), name="runRECA")
-  }
-}
 
 #' @param runfile run with parameters stored in file, and runfiledir as GlovalParameters$resultdir (will be created if does not exist)
 runRECA_file <- function(runfile=NULL, runfiledir=NULL){
@@ -106,4 +20,4 @@ runRECA_file <- function(runfile=NULL, runfiledir=NULL){
 
 #runRECA(projectname, seed=42, export_only = "/Users/a5362/Desktop/torskesett_prepdata_mac.rdata")
 #runRECA_file(runfile = "/Users/a5362/Desktop/torskesett_prepdata_mac.rdata", runfiledir="/Users/a5362/code/github/Rstox_utils/Work/reca")
-runRECA(projectname, seed=42)
+runRECA(projectname, seed=42, burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault)
