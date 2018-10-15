7a1c4060fb57c2dbd1b7d65d7450b81144776610
edvinf <edvin.fuglebakk@gmail.com>
2018-10-15 21:48:34 +0200

    minor

--- Work/install_RECA.R
+++ Work/install_RECA.R
@@ -1,5 +1,5 @@
 
-checkout <- "/Users/a5362/code/nr/ecatest/Test/reca_Version0.6"
+checkout <- "/Users/a5362/code/nr/ecatest/Test/reca_Version0.7"
 
 ## Install new library
 install.packages(checkout,repos = NULL, type="source")
