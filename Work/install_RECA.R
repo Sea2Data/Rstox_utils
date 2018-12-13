ec2464912197b88ca3fd786709be9d46535a428d
edvinf <edvin.fuglebakk@gmail.com>
2018-12-13 08:29:51 +0100

    Updated reca version

--- Work/install_RECA.R
+++ Work/install_RECA.R
@@ -1,5 +1,5 @@
 devtools::install_github("Sea2Data/Rstox", ref="develop")
-checkout <- "/Users/a5362/code/nr/ecatest/Test/reca_Version0.8"
+checkout <- "/Users/a5362/code/nr/ecatest/Test/reca_Version0.10"
 
 ## Install new library
 install.packages(checkout,repos = NULL, type="source")
