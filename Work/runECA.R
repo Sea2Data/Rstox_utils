3bb259dd8d3d22693d42ac1949be3091d2f681bd
edvinf <edvin.fuglebakk@gmail.com>
2018-12-14 22:53:48 +0100

    Added support for log-linear in run script. Prepped bugreport to nr

--- Work/runECA.R
+++ Work/runECA.R
@@ -3,14 +3,15 @@ setJavaMemory(size=10e+09)
 burnindefault=100
 samplesdefault=500
 tempresdefault=92
+lgamodeldefault = "log-linear"
 projectname <- "ECA_torsk_2015"
-#projectname <- "ECA_torsk_2015_cc"w
+#projectname <- "ECA_torsk_2015_cc"
 #projectname <- "ECA_sild_2015"
 #baselineOutput <- getBaseline(projectname)
 
-runproject <- function(projectname, burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault, seed=NULL, tempres=tempresdefault){
+runproject <- function(projectname, burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault, seed=NULL, tempres=tempresdefault, lgamodel=lgamodeldefault){
   prepareRECA(projectname, temporalresolution=tempres)
-  runRECA(projectname, burnin=burnin, caa.burnin=caa.burnin, nSamples=nSamples, seed=seed)
+  runRECA(projectname, burnin=burnin, caa.burnin=caa.burnin, nSamples=nSamples, seed=seed, lgamodel = lgamodel)
   getReports(projectname)
   getPlots(projectname)
 }
@@ -30,7 +31,7 @@ runRECA_file <- function(runfile=NULL, runfiledir=NULL){
   pred <- eca.predict(AgeLength,WeightLength,Landings,GlobalParameters)
 }
 
-#runproject(projectname, tempres = 46)
+runproject(projectname)
 #loadProjectData(projectname, var="prepareRECA")
-#runRECA(projectname, seed=42, export_only = "testfiles/torsk_nonstd_tempres.Rdata", burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault)
+#runRECA(projectname, lgamodel="non-linear", export_only = "bugreports/mactest20181214_reca0.10/sild_nonlinear.Rdata", burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault)
 #runRECA_file(runfile = "bugreports/mactest20181122_reca0.8/length_str.rdata", runfiledir="/Users/a5362/code/github/Rstox_utils/Work/tmp/t")
