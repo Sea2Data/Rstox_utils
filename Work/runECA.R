ce0671ec32c8bca9f44e0a9beaf435606dad8e0e
edvinf <edvin.fuglebakk@gmail.com>
2018-12-14 14:08:38 +0100

    Added option for temporal resolution in landings

--- Work/runECA.R
+++ Work/runECA.R
@@ -1,14 +1,15 @@
 library(Rstox)
-setJavaMemory(size=6e+09)
+setJavaMemory(size=10e+09)
 burnindefault=100
 samplesdefault=500
-#projectname <- "ECA_torsk_2015"
-#projectname <- "ECA_torsk_2015_cc"
-projectname <- "ECA_sild_2015"
+tempresdefault=92
+projectname <- "ECA_torsk_2015"
+#projectname <- "ECA_torsk_2015_cc"w
+#projectname <- "ECA_sild_2015"
 #baselineOutput <- getBaseline(projectname)
 
-runproject <- function(projectname, burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault, seed=NULL){
-  prepareRECA(projectname)
+runproject <- function(projectname, burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault, seed=NULL, tempres=tempresdefault){
+  prepareRECA(projectname, temporalresolution=tempres)
   runRECA(projectname, burnin=burnin, caa.burnin=caa.burnin, nSamples=nSamples, seed=seed)
   getReports(projectname)
   getPlots(projectname)
@@ -29,7 +30,7 @@ runRECA_file <- function(runfile=NULL, runfiledir=NULL){
   pred <- eca.predict(AgeLength,WeightLength,Landings,GlobalParameters)
 }
 
-runproject(projectname)
-#loadProjectData(projectname, var="prepareRECA") 
-#runRECA(projectname, seed=42, export_only = "bugreports/mactest20181024_reca0.8/sild_fails.rdata", burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault)
-#runRECA_file(runfile = "bugreports/mactest20181024_reca0.8/sild_fails.rdata", runfiledir="/Users/a5362/code/github/Rstox_utils/Work/tmp")
+#runproject(projectname, tempres = 46)
+#loadProjectData(projectname, var="prepareRECA")
+#runRECA(projectname, seed=42, export_only = "testfiles/torsk_nonstd_tempres.Rdata", burnin=burnindefault, caa.burnin=burnindefault, nSamples=samplesdefault)
+#runRECA_file(runfile = "bugreports/mactest20181122_reca0.8/length_str.rdata", runfiledir="/Users/a5362/code/github/Rstox_utils/Work/tmp/t")
