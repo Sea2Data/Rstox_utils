aa477020ecf468afff2025e1be0f4582b18a58f8
Holmin <arnejh@HI-10976.hi.no>
2019-01-14 11:04:43 +0100

    Added autumatic recognision of user in build_Rstox.R

--- Work/install_RECA.R
+++ Work/install_RECA.R
@@ -1,4 +1,5 @@
 devtools::install_github("Sea2Data/Rstox", ref="develop")
+# Path to eca R package folder, which must be retrieved from the internal git of NR, which must be done by AJ or Edvin, and copied with a memory stick to e.g. Atle:
 checkout <- "/Users/a5362/code/nr/ecatest/Test/reca_Version0.10"
 
 ## Install new library
