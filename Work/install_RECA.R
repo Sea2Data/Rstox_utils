b97e305adf61a87e73cfc1f4637069cd09d8ea83
edvinf <edvin.fuglebakk@gmail.com>
2019-04-04 10:53:04 +0200

    Updated install instructions

--- Work/install_RECA.R
+++ Work/install_RECA.R
@@ -1,9 +1,10 @@
-devtools::install_github("Sea2Data/Rstox", ref="develop")
+#devtools::install_github("Sea2Data/Rstox", ref="develop")
+#devtools::install_github("NorskRegnesentral/Reca", ref="master")
 
 #
+# Mac
 # point to local copy of public eca version (https://github.com/NorskRegnesentral/Reca)
+# compile for Mac
 #
-checkout <- "/Users/a5362/code/github/public_reca"
-
-## Install new library
-install.packages(checkout,repos = NULL, type="source")
+# checkout <- "/Users/a5362/code/github/public_reca"
+# install.packages(checkout,repos = NULL, type="source")
