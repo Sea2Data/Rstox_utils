c4ddedab2eb96040973813764a67fde7d4deba32
edvinf <edvin.fuglebakk@gmail.com>
2019-04-25 15:28:46 +0200

    Added script for merging polygons

---
+++ merge_polygons.R
@@ -0,0 +1,44 @@
+library(raster)
+
+#' @param shapes SpatialPolygonsDataFrama
+#' @param groups list mapping new names to groups of old names as identified in shapes by namecol
+#' @param namecol column identifying names of areas
+merge_polygons <- function(shapes, groups, namecol){
+  
+  names <- c()
+  newpolygons <- NULL
+  for (g in names(groups)){
+    newname <- g
+    oldnames <- groups[[newname]]
+    
+    polygons <- shapes[shapes[[namecol]] %in% oldnames,]
+    newpolygon <- raster::aggregate(polygons)
+
+    if (is.null(newpolygons)){
+      newpolygons <- newpolygon
+    }
+    else{
+      newpolygons <- union(newpolygons, newpolygon)
+    }
+    
+    names <- c(names, newname)
+  }
+  
+  names <- as.data.frame(list(names))
+  names(names) <- namecol
+  
+  
+  return(SpatialPolygonsDataFrame(newpolygons, names))
+}
+
+# Can be used for recasting the ICES fom 2018 areas, but it needs some cleaning of suspicious features first
+# make mappiing for recasting:
+# ll<-as.list(as.character(unique(ices_fom2018_all$SubArea)))
+# names(ll) <- unlist(ll)
+
+#
+# area merge for RECA kolmule 2017, based on Sondre Aanes parameter file for the old eca
+#
+kolmulemapping <- list()
+kolmulemapping[["allareas"]] <- c("08", "09", "28", "41", "42", "04", "05", "06", "07", "12", "30", "31", "34", "36", "37", "43", "47", "48", "49")
+kolmule2017 <- merge_polygons(homr_tom2017, kolmulemapping, "HAVOMR")
