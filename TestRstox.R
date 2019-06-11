34657cdea6eae802af52807e7db5f434a65bce9b
Holmin <arnejh@HI-10976.hi.no>
2019-06-11 12:33:28 +0200

    New TestRstox.R

--- TestRstox.R
+++ TestRstox.R
@@ -13,97 +13,6 @@ install.packages(c("png", "jpeg", "tiff", "R.utils", "Rcpp", "rlang"))
 
 
 
-###############################
-########## Rstox_1.8 ##########
-###############################
-
-###### RESTART R/Rstudio!!! ######
-
-###### Install Rstox_1.8: #####
-
-# Install the packages that Rstox depends on. Note that this updates all the specified packages to the latest (binary) version:
-dep.pck <- c("data.table", "ggplot2", "pbapply", "rgdal", "rgeos", "rJava", "sp", "XML")
-install.packages(dep.pck, repos="http://cran.us.r-project.org", type="binary")
-
-# Install Rstox:
-install.packages("ftp://ftp.imr.no/StoX/Download/Rstox/Versions/Rstox_1.8/Rstox_1.8.tar.gz", repos=NULL)
-# Load Rstox:
-library(Rstox)
-
-###### Run the projects with Rstox 1.8 with no diff: #####
-source("https://raw.githubusercontent.com/Sea2Data/Rstox_utils/master/Rstox_utils.R")
-
-system.time(automatedRstoxTest(copyFromServer=TRUE, process=c("run", "diff"), nlines=50))
-#     user   system  elapsed 
-# 1156.712  271.704 3834.652 
-
-
-#################################
-########## Rstox_1.8.1 ##########
-#################################
-
-###### RESTART R/Rstudio!!! ######
-
-##### Install the latest develop version of Rstox: #####
-# Install the packages that Rstox depends on. Note that this updates all the specified packages to the latest (binary) version:
-dep.pck <- c("data.table", "ggplot2", "pbapply", "rgdal", "rgeos", "rJava", "sp", "XML")
-install.packages(dep.pck, repos="http://cran.us.r-project.org", type="binary")
-
-# Install Rstox:
-install.packages("ftp://ftp.imr.no/StoX/Download/Rstox/Versions/Alpha/Rstox_1.8.1/Rstox_1.8.1.tar.gz", repos=NULL)
-
-# Load Rstox:
-library(Rstox)
-
-##### Run the projects with Rstox 1.8.1 and diff with Rstox 1.8: #####
-source("https://raw.githubusercontent.com/Sea2Data/Rstox_utils/master/Rstox_utils.R")
-
-system.time(automatedRstoxTest(copyFromServer=TRUE, process=c("run", "diff"), nlines=50))
-#     user   system  elapsed 
-# 2044.219  394.894 4639.868 
-
-
-#################################
-########## Rstox_1.9.4 ##########
-#################################
-
-###### RESTART R/Rstudio!!! ######
-
-##### Install the latest develop version of Rstox: #####
-# Install the packages that Rstox depends on. Note that this updates all the specified packages to the latest (binary) version:
-dep.pck <- c("data.table", "ggplot2", "pbapply", "rgdal", "rgeos", "rJava", "sp", "XML")
-install.packages(dep.pck, repos="http://cran.us.r-project.org", type="binary")
-
-# Install Rstox:
-install.packages("ftp://ftp.imr.no/StoX/Download/Rstox/Versions/Alpha/Rstox_1.9.4/Rstox_1.9.4.tar.gz", repos=NULL)
-
-# Load Rstox:
-library(Rstox)
-
-# Install Reca on Windows (on Mac there are several steps required, see GitHub, "Rstox_utils/Work/mac_binaries/README.txt"):
-###devtools::install_github("NorskRegnesentral/Reca")
-# Install the Reca package you got via Wetransfer:
-# ecafolder <- file.path(getProjectPaths()$projectRoot, "eca_0.11")
-# install.packages(ecafolder, repos=NULL, type="source")
-
-
-##### Run the projects with Rstox 1.9.4 and diff with Rstox 1.8.1: #####
-source("https://raw.githubusercontent.com/Sea2Data/Rstox_utils/master/Rstox_utils.R")
-#source('~/Code/Github/Rstox_utils/Rstox_utils/Rstox_utils.R', chdir = TRUE)
-
-
-# system.time(automatedRstoxTest(dir="~/workspace/stox/project/Automated_testing", copyFromServer=TRUE, process=c("run", "diff"), nlines=50))
-system.time(automatedRstoxTest(copyFromServer=TRUE, process=c("run", "diff"), nlines=50, skipError=TRUE))
-#     user   system  elapsed 
-# 2044.219  394.894 4639.868 
-# New mac
-#     user   system  elapsed 
-# 3030.232  306.394 2742.444 
-
-
-
-
-
 #################################
 ########## Rstox_1.10.1 ##########
 #################################
@@ -117,9 +26,11 @@ install.packages(dep.pck, repos="http://cran.us.r-project.org", type="binary")
 
 # Install Rstox:
 install.packages("ftp://ftp.imr.no/StoX/Download/Rstox/Versions/Alpha/Rstox_1.10.1/Rstox_1.10.1.tar.gz", repos=NULL)
+devtools::install_github("https://github.com/StoXProject/RstoxBuild")
 
 # Load Rstox:
 library(Rstox)
+library(RstoxBuild)
 
 # Install Reca on Windows (on Mac there are several steps required, see GitHub, "Rstox_utils/Work/mac_binaries/README.txt"):
 ###devtools::install_github("NorskRegnesentral/Reca")
@@ -151,6 +62,7 @@ install.packages(dep.pck, repos="http://cran.us.r-project.org", type="binary")
 
 # Install Rstox:
 devtools::install_github("Sea2Data/Rstox", ref="develop")
+devtools::install_github("https://github.com/StoXProject/RstoxBuild")
 
 # Load Rstox:
 library(Rstox)
@@ -161,11 +73,6 @@ library(RstoxBuild)
 # Install the Reca package you got via Wetransfer:
 # ecafolder <- file.path(getProjectPaths()$projectRoot, "eca_0.11")
 # install.packages(ecafolder, repos=NULL, type="source")
-
-
-##### Run the projects with Rstox 1.10.1 and diff with Rstox 1.9.4: #####
-#source("https://raw.githubusercontent.com/Sea2Data/Rstox_utils/master/Rstox_utils.R")
-#source('~/Code/Github/Rstox_utils/Rstox_utils/Rstox_utils.R', chdir = TRUE)
 
 
 # system.time(automatedRstoxTest(dir="~/workspace/stox/project/Automated_testing", copyFromServer=TRUE, process=c("run", "diff"), nlines=50))
