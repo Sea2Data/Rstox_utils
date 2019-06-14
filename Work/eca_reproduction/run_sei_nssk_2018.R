95ab62e5a95e3859b27e400e554449edddc78ced
edvinf <edvin.fuglebakk@gmail.com>
2019-06-14 14:54:47 +0200

    Revised some stox estimates

--- Work/eca_reproduction/run_sei_nssk_2018.R
+++ Work/eca_reproduction/run_sei_nssk_2018.R
@@ -1,6 +1,7 @@
 source("run_project.R")
+#correct snapshot for landings not available through API for this estimate. Handle manually
 bioticurl <- "http://tomcat7-test.imr.no:8080/apis/nmdapi/biotic/v3/2018/164727/search?version=3.0"
-landingurl <- "http://tomcat7.imr.no:8080/apis/nmdapi/landing/v2?version=2.0&type=search&Art_kode=1032&Fangstar=2018"
+landingurl <- NULL # "http://tomcat7.imr.no:8080/apis/nmdapi/landing/v2?version=2.0&type=search&Art_kode=1032&Fangstar=2018"
 projectfile <- "sei_nssk/stox_2018/project.xml"
 projectname <- "ECA_NSSK_sei_2018"
 resultdest <- "sei_nssk/stox_2018/results/"
@@ -9,4 +10,5 @@ resultdest <- "sei_nssk/stox_2018/results/"
 # createStoxProject(files$biotic, files$landing)
 
 # createStoxProject not working yet, set up manuall for now
+setJavaMemory(size=10e+09)
 runProject(projectname)
--- Work/eca_reproduction/run_torsk_nssk_2018.R
+++ Work/eca_reproduction/run_torsk_nssk_2018.R
@@ -1,4 +1,5 @@
 source("run_project.R")
+#correct snapshot for landings not available through API for this estimate. Handle manually
 bioticurl <- ""
 landingurl <- ""
 projectfile <- "torsk_nssk/stox_2018/project_IV.xml"
