ea5959591b97b1401e18ebda8224ccf592870f82
edvinf <edvin.fuglebakk@gmail.com>
2019-06-18 01:03:29 +0200

    Updatest to reprod report

--- Work/stratafile_generation/makestratafiles.R
+++ Work/stratafile_generation/makestratafiles.R
@@ -11,6 +11,16 @@ kolmule2017 <- merge_polygons(homr_tom2017, kolmulemapping, "HAVOMR")
 convertToWKT(kolmule2017, file.path("output", "RECA_kolmule_2017_stratafile.txt"), "komule2017")
 
 #
+# area merge for RECA makrell 2018, based on Sondre Aanes parameter file for ECA 3.x
+#
+makrellmapping <- list()
+makrellmapping[["nord"]] <- c("00", "04", "05", "06", "07", "12", "27", "30", "34", "36", "37", "38", "39")
+makrellmapping[["sor"]] <- c("08", "09", "28", "41", "42", "43", "48", "57")
+makrell2018 <- merge_polygons(homr_tom2017, makrellmapping, "HAVOMR")
+convertToWKT(makrell2018, file.path("output", "RECA_makrell_2018_stratafile.txt"), "HAVOMR")
+makrell2018_neighbours <- extract_neighbours(makrell2018, namecol="HAVOMR")
+save_neighbours(makrell2018_neighbours, file.path("output", "RECA_makrell_2018_neighbours.txt"))
+#
 # area merge for RECA nssk 2018, based on Åge Fotlands parameter file for the old eca
 #
 nssk_mapping <- list()
