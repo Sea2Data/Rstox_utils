a4feae4ae3d74b2b53fe4b5b80975b60d2f199e8
edvinf <edvin.fuglebakk@gmail.com>
2019-10-17 20:10:52 +0200

    fixed wrong urls, referring test server

--- Work/eca_reproduction/run_makrell.r
+++ Work/eca_reproduction/run_makrell.r
@@ -1,5 +1,5 @@
 source("run_project.R")
-bioticurl <- "http://tomcat7-test.imr.no:8080/apis/nmdapi/biotic/v3/2018/172414/search?version=3.0"
+bioticurl <- "http://tomcat7.imr.no:8080/apis/nmdapi/biotic/v3/2018/172414/search?version=3.0"
 landingurl <- "http://tomcat7.imr.no:8080/apis/nmdapi/landing/v2?version=2.0&type=search&Art_kode=2013&Fangstar=2018"
 projectfile <- "makrell/stox_2018/project.xml"
 projectname <- "ECA_makrell_2018"
--- Work/eca_reproduction/run_sei_nssk_2018.R
+++ Work/eca_reproduction/run_sei_nssk_2018.R
@@ -1,6 +1,6 @@
 source("run_project.R")
 #correct snapshot for landings not available through API for this estimate. Handle manually
-bioticurl <- "http://tomcat7-test.imr.no:8080/apis/nmdapi/biotic/v3/2018/164727/search?version=3.0"
+bioticurl <- "http://tomcat7.imr.no:8080/apis/nmdapi/biotic/v3/2018/164727/search?version=3.0"
 landingurl <- NULL # "http://tomcat7.imr.no:8080/apis/nmdapi/landing/v2?version=2.0&type=search&Art_kode=1032&Fangstar=2018"
 projectfile <- "sei_nssk/stox_2018/project.xml"
 projectname <- "ECA_NSSK_sei_2018"
